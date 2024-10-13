fe64b69a0893f3f0f84272b81cd140715ea79b09
e1uen <110753825+e1uen@users.noreply.github.com>
2024-10-13 20:37:26 +0700

    Update script.bat
    Signed-off-by: e1uen <110753825+e1uen@users.noreply.github.com>

--- DelusionOS/src/Post-Script/24H2/script.bat
+++ DelusionOS/src/Post-Script/24H2/script.bat
@@ -14,17 +14,6 @@ DISM >nul || (
 :: Delusion LLC
 :: working for script - hickerdicker, couwthynokap, clqwnless, e1uen
 :: license Attribution-NonCommercial 4.0 International
-:: Hello,
-:: Before I start, I would like to thank the following peoplers who helped me develop this project.
-:: Sharing your experiences and results github, twitter, discord
-:: clqwnless / https://github.com/clqwnless
-:: couwthynokap / https://github.com/couwthynokap
-:: hickerdicker / https://github.com/hickerdicker
-:: imribiy / https://github.com/imribiy
-:: ChrisTitusTech / https://github.com/ChrisTitusTech
-:: AMITXV / https://github.com/amitxv
-:: Catgamerop / https://x.com/CatGamerOP
-:: Spddl / https://github.com/spddl
 
 call :Colors
 
