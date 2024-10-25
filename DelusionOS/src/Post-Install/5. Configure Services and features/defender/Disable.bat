b6ff6ad5961492b74e502807bae9bbf8622b3559
e1uen <110753825+e1uen@users.noreply.github.com>
2024-10-25 20:50:49 +0700

    Delete DelusionOS/src/Post-Install/5. Configure Services and features directory
    Signed-off-by: e1uen <110753825+e1uen@users.noreply.github.com>

--- DelusionOS/src/Post-Install/5. Configure Services and features/defender/Disable.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Disabling Defender
-
-"%ProgramFiles%\Windows Defender\MpCmdRun.exe" -DisableService
-reg add "HKLM\SOFTWARE\Microsoft\Windows Defender\Features" /v "TamperProtection" /t REG_DWORD /d "0" /f >nul
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\WdNisSvc" /v "Start" /t REG_DWORD /d "4" /f >nul
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\MDCoreSvc" /v "Start" /t REG_DWORD /d "4" /f >nul
-sc config wscsvc start=disabled & sc stop wscsvc
