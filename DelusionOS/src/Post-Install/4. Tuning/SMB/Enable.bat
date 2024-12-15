854e397c6571962dee918f76e4e49f9559f6363c
e1uen <110753825+e1uen@users.noreply.github.com>
2024-12-15 11:18:34 +0700

    Update Enable.bat
    Signed-off-by: e1uen <110753825+e1uen@users.noreply.github.com>

--- DelusionOS/src/Post-Install/4. Tuning/SMB/Enable.bat
+++ DelusionOS/src/Post-Install/4. Tuning/SMB/Enable.bat
@@ -2,9 +2,19 @@
 title Improving security with SMB...
 
 echo Please write for A (to ALL apply)
-powershell -Command "Enable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol"
+powershell -Command "Enable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol" -NoRestart
 
 echo Please write for A (to ALL apply)
 powershell -Command "Set-SmbServerConfiguration -EnableSMB1Protocol $true"
+@echo off
+title Disabling security Protocol SMB1...
+
+echo Please write for A (to ALL apply)
+powershell -Command "Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol"
+
+echo Please write for A (to ALL apply)
+powershell -Command "Set-SmbServerConfiguration -EnableSMB1Protocol $false"
+sc.exe config lanmanworkstation depend= bowser/mrxsmb10/mrxsmb20/nsi
+sc.exe config mrxsmb10 start= auto
 
 shutdown /r /t 3
